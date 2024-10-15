dd1e3954d946f3a289f7428be5fa18aad6fca9ff
Ahmed Ramadan Nazeer <ahmed.nazeer.qotb@gmail.com>
2024-10-15 11:11:29 +0300

    initial commit

---
+++ src/app/components/best-saller/best-saller.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-saller',
+  standalone: true,
+  imports: [],
+  templateUrl: './best-saller.component.html',
+  styleUrl: './best-saller.component.css'
+})
+export class BestSallerComponent {
+
+}
